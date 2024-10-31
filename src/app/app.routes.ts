637fd3d573d25bf815d408295326b35a7426ee13
Francisco León <22012000fdlq@gmail.com>
2024-10-30 22:09:46 -0300

    Instalacion Bootstrap, font awesome, creacion components y base login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from '../auth/login/login.component';
+import { ResetPasswordComponent } from '../auth/reset-password/reset-password.component';
+import { CreateTicketComponent } from '../dashboard/create-ticket/create-ticket.component';
+import { ViewTicketsComponent } from '../dashboard_TI/view-tickets/view-tickets.component';
+import { RegisterUsersComponent } from '../admin_users/register-users/register-users.component';
+
+
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'reset-password', component: ResetPasswordComponent },
+    { path: 'dashboard/create-ticket', component: CreateTicketComponent },
+    { path: 'dashboard_TI/view-tickets', component: ViewTicketsComponent },
+    { path: 'admin/register-users', component: RegisterUsersComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },  // Redirección inicial a login
+    { path: '**', redirectTo: '/login' }  // Redirección a login si la ruta no existe
+];
---
+++ src/dashboard_TI/view-tickets/view-tickets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-tickets.component.html',
+  styleUrl: './view-tickets.component.css'
+})
+export class ViewTicketsComponent {
+
+}
